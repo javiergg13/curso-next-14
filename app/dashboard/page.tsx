d79b2804eeabc4fc8fcb669bd2ee32f504113ead
jguerrero <jguerrero@cgi.es>
2023-11-15 12:46:51 +0100

    v1 suspense, routes, skeletons y más

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,34 @@
-export default function Page() {
+import { Suspense } from 'react'
+import { fetchLatestInvoices } from '../lib/data'
+import LatestInvoices from '../ui/dashboard/latest-invoices'
+import RevenueChart from '../ui/dashboard/revenue-chart'
+import { lusitana } from '../ui/fonts'
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons'
+
+export default async function Page() {
+  
   return (
-    <div>
-      <h1>Page</h1>
-    </div>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+
+
+      </div>
+
+      <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        
+      </div>
+
+    </main>
   )
 }
